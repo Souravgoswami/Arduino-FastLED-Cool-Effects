f5f6fedd59da6f3e316e03161d8858d88ea86588
Souravgoswami <souravgoswami@protonmail.com>
2021-11-28 05:26:28 +0530

    New effects

---
+++ patterns/arrayFunctions.h
@@ -0,0 +1,32 @@
+void arrayRotate(unsigned long *ary, unsigned char length) {
+	length-- ;
+	unsigned long first = ary[0] ;
+
+	for(unsigned char i = 0 ; i < length ; ++i)
+		ary[i] = ary[i + 1] ;
+
+	ary[length] = first ;
+}
+
+void arrayShuffle(unsigned long *ary, unsigned char length) {
+	if (length > 1) {
+		unsigned char length_minus_one = length - 1 ;
+
+		for(unsigned char i = 0 ; i < length_minus_one ; ++i) {
+			unsigned char j = i + rand() / (RAND_MAX / (length - i) + 1) ;
+
+			unsigned long temp = ary[j] ;
+			ary[j] = ary[i] ;
+			ary[i] = temp ;
+		}
+	}
+}
+
+// void rotateColoursAnim(unsigned long *colours, unsigned char length) {
+// 	for(unsigned char i = 0 ; i < NUM_LEDS ; ++i) {
+// 		leds[i] = colours[0] ;
+// 		arrayRotate(colours, length) ;
+// 		FastLED.show() ;
+// 		delay(10) ;
+// 	}
+// }
---
+++ patterns/random.h
@@ -0,0 +1,53 @@
+void generateRandomColours(unsigned long *ary) {
+	for(unsigned char i = 0 ; i < NUM_LEDS ; ++i) {
+		unsigned long red = rand() & 255 ;
+		unsigned long green = rand() & 255 ;
+		unsigned long blue = rand() & 255 ;
+
+		unsigned long colour = blue + (256 * green) + (256L * 256L * red) ;
+		ary[i] = colour ;
+	}
+}
+
+void randomColoursDelayed(unsigned long *ary) {
+	for(unsigned char i = 0 ; i < NUM_LEDS ; ++i) {
+		leds[i] = ary[i] ;
+		FastLED.show() ;
+		delay(20) ;
+	}
+}
+
+void randomColours(unsigned long *ary) {
+	for(unsigned char i = 0 ; i < NUM_LEDS ; ++i) {
+		leds[i] = ary[i] ;
+	}
+
+	FastLED.show() ;
+}
+
+void setBrightColours(const unsigned long *ary, unsigned char length) {
+	unsigned char colIndex = 0 ;
+
+	for(unsigned char i = 0 ; i < NUM_LEDS ; ++i) {
+		if (colIndex == length) colIndex = 0 ;
+		leds[i] = ary[colIndex] ;
+		colIndex += 1 ;
+	}
+
+	FastLED.show() ;
+}
+
+void rotateBrightColours(unsigned long *ary, unsigned char length) {
+	unsigned char colIndex = 0 ;
+
+	for(unsigned char i = 0 ; i < NUM_LEDS ; ++i) {
+		if (colIndex == length) colIndex = 0 ;
+		leds[i] = ary[colIndex] ;
+		colIndex += 1 ;
+
+		delay(10) ;
+		FastLED.show() ;
+
+		arrayRotate(ary, length) ;
+	}
+}
