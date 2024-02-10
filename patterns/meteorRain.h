2b3518f96524aef37c26c4757b2633227e6e1752
Souravgoswami <souravgoswami@protonmail.com>
2024-02-10 14:17:39 +0530

    meteorRain: Fix types and remove __attribute__((packed))

--- patterns/meteorRain.h
+++ patterns/meteorRain.h
@@ -2,35 +2,31 @@ struct MetorRainData {
   float meteorSizeFactor = 0.2; // 20 /  100
   float meteorTrailDecayFactor = 0.5; // 64 / 100
   float meteorTrailLengthFactor = 0.25; // 25 / 100
-  unsigned short meteorDelayFactor = 1000; // 10 * 100
-
+  uint16_t meteorDelayFactor = 1000; // 10 * 100
   bool meteorRandomDecay = true;
+} metorRainData;
 
-} __attribute__((packed));
+void meteorRain(uint32_t colour, uint16_t numLED) {
+  uint16_t meteorSize = max(1, (int)(numLED * metorRainData.meteorSizeFactor));
+  uint8_t meteorTrailDecay = max(1, (int)(255 * metorRainData.meteorTrailDecayFactor));
+  uint16_t meteorTrailLength = max(1, (int)(numLED * metorRainData.meteorTrailLengthFactor));
 
-MetorRainData metorRainData;
-
-void meteorRain(uint32_t colour, unsigned short numLED) {
-  unsigned short meteorSize = max(1, (int)(numLED * metorRainData.meteorSizeFactor));
-  unsigned char meteorTrailDecay = max(1, (int)(255 * metorRainData.meteorTrailDecayFactor));
-  unsigned short meteorTrailLength = max(1, (int)(numLED * metorRainData.meteorTrailLengthFactor));
-
-  unsigned char r = colour >> 16;
-  unsigned char g = (colour >> 8) & 0xff;
-  unsigned char b = colour & 0xff;
+  uint8_t r = colour >> 16;
+  uint8_t g = (colour >> 8) & 0xff;
+  uint8_t b = colour & 0xff;
 
   FastLED.clear();
 
-  for (unsigned short i = 0; i < numLED + meteorTrailLength; ++i) {
+  for (uint16_t i = 0; i < numLED + meteorTrailLength; ++i) {
     // Draw the meteor
-    for (unsigned short j = 0; j < meteorSize; ++j) {
+    for (uint16_t j = 0; j < meteorSize; ++j) {
       if (i - j < numLED) {
         leds[i - j] = CRGB(r, g, b);
       }
     }
 
     // Draw the tail
-    for (unsigned short j = 0; j < numLED; ++j) {
+    for (uint16_t j = 0; j < numLED; ++j) {
       if (metorRainData.meteorRandomDecay && (random(10) > 3)) {
         leds[j].fadeToBlackBy(meteorTrailDecay);
       }
